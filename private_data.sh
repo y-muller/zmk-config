97eebce17706528a0e627e7b029e16b4c6668f3f
Yann Muller <yann.muller@gmail.com>
2024-12-08 22:39:46 +0100

    secrets experiment 3

--- private_data.sh
+++ private_data.sh
@@ -5,5 +5,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PRIVATE_PASSPHRASE" \
---output $HOME/config/private_macros.dtsi $HOME/config/private_macros.dtsi.gpg
+--output ./config/private_macros.dtsi ./config/private_macros.dtsi.gpg
 
